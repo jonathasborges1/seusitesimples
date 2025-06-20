357f96c493d6ca2debc3c8b3b2b8a51d6fcd5650
Jonathas Borges <jbc@icomp.ufam.edu.br>
2025-06-20 10:29:44 -0400

    fix: deploy cloudflare #003 - hotfix added next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export", // Necessário para Cloudflare Pages
+  distDir: "out", // Define o diretório de saída para "out"
+  compress: true, // Gzip habilitado
+  images: {
+    unoptimized: true, // Obrigatorio para funcionar
+  },
 };
 
 export default nextConfig;
