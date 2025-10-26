76317d4533000b68ceb242d3f3ee165997b8d375
Jonathas Borges <jbc@icomp.ufam.edu.br>
2025-10-26 17:39:17 -0400

    refactor: added meta tag  google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,9 @@ export const metadata = {
     siteName: "Seu Site Simples",
     type: "website",
     locale: "pt_BR",
+    verification: {
+      google: "-aEdvna45HfuoQzvEtZiYF0J6Q6snzapEc1lmIuTmGU",
+    },
   },
 };
 
